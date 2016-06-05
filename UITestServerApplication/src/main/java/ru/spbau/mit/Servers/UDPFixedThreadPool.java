a2e55a413d94fccc642663cc8ec756c5bb9eed6b
nast1415 <nast1415@gmail.com>
2016-06-05 22:34:20 +0300

    Add fixedThreadPool

--- UITestServerApplication/src/main/java/ru/spbau/mit/Servers/UDPFixedThreadPool.java
+++ UITestServerApplication/src/main/java/ru/spbau/mit/Servers/UDPFixedThreadPool.java
@@ -17,27 +17,94 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class UDPFixedThreadPool extends BaseServer {
+    private static final int TIMEOUT = 10000;
+    private static final int MAX_SIZE = 50000;
+
+    ExecutorService fixedThreadPool = Executors.newFixedThreadPool(4);
+
+    private AtomicLong summaryRequestsTime = new AtomicLong(0);
+    private AtomicInteger numberOfRequests = new AtomicInteger(0);
+
+    private AtomicLong summaryClientsTime = new AtomicLong(0);
+
+    DatagramSocket socket;
+
     public UDPFixedThreadPool(int numberOfClients) {
         super(numberOfClients);
     }
 
     @Override
     public void start() throws IOException {
-        throw new UnsupportedOperationException();
+        new Thread(this::handleConnections).start();
+    }
+
+    private void handleConnections() {
+        System.err.println("start");
+        try {
+            socket = new DatagramSocket(8081);
+            socket.setSoTimeout(TIMEOUT);
+            while (!socket.isClosed()) {
+                System.err.println("notClosed!!!");
+                summaryClientsTime.getAndAdd(-System.currentTimeMillis());
+                summaryRequestsTime.getAndAdd(-System.currentTimeMillis());
+
+                byte[] receivedData = new byte[MAX_SIZE];
+                DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+                socket.receive(receivedPacket);
+
+                ByteBuffer receivedBuffer = ByteBuffer.wrap(receivedData);
+                int size = receivedBuffer.getInt();
+                byte[] data = new byte[size];
+                receivedBuffer.get(data);
+                ArrayProto.Array arrayToSort = ArrayProto.Array.parseFrom(data);
+
+                ArrayProto.Array resultArray = sort(arrayToSort);
+                System.err.println("len: " + resultArray.getElementCount());
+
+                ByteBuffer bufferToSend = ByteBuffer.allocate(4 + resultArray.getSerializedSize());
+                bufferToSend.putInt(resultArray.getSerializedSize());
+                bufferToSend.put(resultArray.toByteArray());
+                byte[] dataToSend = bufferToSend.array();
+
+                fixedThreadPool.execute(() -> {
+                    try {
+                        System.err.println("Server send");
+                        DatagramPacket packet = new DatagramPacket(dataToSend, dataToSend.length,
+                                receivedPacket.getSocketAddress());
+                        socket.send(packet);
+                        System.err.println("Server sent");
+
+                    } catch (IOException ignored) {
+                    } finally {
+                        summaryClientsTime.getAndAdd(System.currentTimeMillis());
+                        summaryRequestsTime.getAndAdd(System.currentTimeMillis());
+                        numberOfRequests.getAndIncrement();
+                    }
+                });
+
+            }
+            System.err.println("Server closed");
+        } catch (IOException ignored) {
+            ignored.printStackTrace();
+            summaryClientsTime.getAndAdd(System.currentTimeMillis());
+            summaryRequestsTime.getAndAdd(System.currentTimeMillis());
+            numberOfRequests.getAndIncrement();
+        }
     }
 
     @Override
     public void stop() throws IOException {
-        throw new UnsupportedOperationException();
+        System.err.println("close server");
+        socket.close();
     }
 
     @Override
     public int getQueryTime() {
-        return 1;
+        return (int) (summaryRequestsTime.get() / numberOfRequests.get());
     }
 
     @Override
     public long getSummaryClientsTime() {
-        return 1;
+        return summaryClientsTime.get() / numberOfClients;
     }
 }
